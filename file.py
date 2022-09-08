aeaf174b6eb5e2274339d78635b59e4d6e51edef
Viktor <fokus11548@mail.ru>
2022-09-08 22:29:55 +0500

    Добавил цикл, который считает сумму массива

--- file.py
+++ file.py
@@ -1,1 +1,13 @@
 print('Hello world')
+
+# Сумма массива
+numbers = [2, 5, 13, 7, 6, 4]
+size = 6
+sum = 0
+avg = 0
+index = 0
+while index < size:
+    sum = sum + numbers[index]
+    index += 1
+avg = sum/size
+print(avg)
