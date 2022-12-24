2b6f9a515a5b69951c8cb18837e8087b4bfc038d
Pavel <sivec_p@mail.ru>
2022-12-24 16:32:28 +0300

    add check dubl password for registration

--- file.py
+++ file.py
@@ -1,1 +1,11 @@
 print('Hello world')
+
+n, d = int(input()), {}
+for _ in range(n):
+    password = input()
+    if password not in d:
+        print('Password is available')
+        d[password] = 1
+    else:
+        print(f'Choose another password, for example: {password + str(d[password])}')
+        d[password] += 1
