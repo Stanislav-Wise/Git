97d6e9423eb313a717e4de55b58062f39375cd10
maxim <maximus77t@gmail.com>
2023-01-05 22:08:50 +0300

    All done"

--- file.py
+++ file.py
@@ -3,4 +3,4 @@ print('Hello world')
 Somebody once told me the world is gonna roll me
 I ain't the sharpest tool in the shed
 
-![This is Shrek](http://almode.ru/uploads/posts/2021-04/1618390728_52-p-tatu-shrek-60.jpg)
+Shrek was supposed to be here, but he left:(
