9188c0626d3d03ba05ce78fa89afbebe60a1becf
Ilya-Lugovoy <121395392+Ilya-Lugovoy@users.noreply.github.com>
2023-01-07 10:50:33 +0300

    Stanislav, Happy New Year to you!

--- file.py
+++ file.py
@@ -1,1 +1,10 @@
 print('Hello world')
+
+## Поиск первой пары подряд положительных, либо отрицательных чисел
+array = [2, -32, -1, -1, 41, -5, -7, 6]
+ind = 1
+while ind < len(array):
+    if (array[ind] * array[ind - 1]) > 0:
+        print(array[ind - 1], array[ind])
+        break
+    ind += 1
