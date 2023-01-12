e92243668b73290fc3f9f95cd0244eb65a21201d
ArturFarukshin <122519255+ArturFarukshin@users.noreply.github.com>
2023-01-12 19:06:37 +0300

    добавил фото

--- file.py
+++ file.py
@@ -1,2 +1,4 @@
 print('Hello world')
 print("HELLO Stas!")
+
+![formula1](https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Jacques_Villeneuve_%28Sauber%29_qualifying_at_US_Grand_Prix_2005.jpg/330px-Jacques_Villeneuve_%28Sauber%29_qualifying_at_US_Grand_Prix_2005.jpg)
