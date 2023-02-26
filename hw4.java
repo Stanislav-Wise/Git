61c70cfb4a9c53a5c1d1ff16aa6d9970ff37cdab
Tsepelev Yuri <foxsidark@yandex.ru>
2023-02-26 19:05:25 +0300

    seminar4

---
+++ hw4.java
@@ -0,0 +1,73 @@
+/**
+ * Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+ */
+
+import java.util.ArrayDeque;
+
+//public class hw4 {
+
+  //  public static ArrayDeque<Integer> getReversDeque(int count) {
+    //    ArrayDeque<Integer> deque = new ArrayDeque<>();
+      //  ArrayDeque<Integer> reverseDeque = new ArrayDeque<>();
+
+        //for (int i = 0; i < count; i++) {
+          //  int rand = (int) (Math.random() * (100 - 1));
+            //deque.addFirst(rand);
+//        }
+  //      System.out.println(deque);
+
+    //    for (int i = 0; i < count; i++) {
+      //      reverseDeque.addFirst(deque.getFirst());
+        //    deque.removeFirst();
+//        }
+  //      return reverseDeque;
+    //}
+
+//    public static void main(String[] args) {
+
+  //      System.out.println(getReversDeque(10));
+
+    //}
+
+//}
+ 
+//Реализуйте очередь с помощью LinkedList со следующими методами:
+  // enqueue() - помещает элемент в конец очереди, dequeue() - возвращает первый элемент из очереди и удаляет его, 
+   //first() - возвращает первый элемент из очереди, не удаляя.
+ 
+
+//public class hw4 {
+
+  //  public static ArrayDeque<Integer> enqueue(ArrayDeque<Integer> linkedList, int element) {
+    //    linkedList.addLast(element);
+
+      //  return linkedList;
+//    }
+
+  //  public static int dequeue(ArrayDeque<Integer> linkedList) {
+    //    int firstElement = linkedList.getFirst();
+      //  linkedList.pollFirst();
+        //System.out.println("Очередь после удаления элемента: " + linkedList);
+
+//        return firstElement;
+  //  }
+
+    //public static int first(ArrayDeque<Integer> linkedList) {
+      //  return linkedList.getFirst();
+    //}
+
+//    public static void main(String[] args) {
+  //      ArrayDeque<Integer> linkedList = new ArrayDeque<>();
+    //    for (int i = 0; i < 10; i++) {
+      //      linkedList.add((int) (Math.random() * (10 - 1)));
+        //}
+//        int element = 22;
+  //      System.out.println("Очередь: " + linkedList);
+   //     System.out.println("Очередь после добавления элемента: " + enqueue(linkedList, element) + "\n"
+     //           + "Добавляемый элемент " + element);
+       // System.out.println("Удаляемый элемент: " + dequeue(linkedList));
+        //System.out.println("Первый элемент: " + first(linkedList));
+    //}
+
+//}
+
